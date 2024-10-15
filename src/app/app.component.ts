553b2daa8a32a9c5c7aedac0a9d5c7c211732435
Vero <vermonca@hotmail.com>
2024-10-15 18:43:43 +0200

    Subidos los archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.jpg" alt="logo">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
